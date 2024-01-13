172ee3c5880b159402152b925979998535419d90
imrank00 <113467979+imrank00@users.noreply.github.com>
2024-01-13 20:13:21 +0530

    weather

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "5bcbec53b5b493d144266d19c18e08f9",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
